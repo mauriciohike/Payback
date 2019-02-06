cebf4c14e9b3321138eecf9f1c733496f6502c53
mauriciohike <mauriciorenanvs@gmail.com>
2019-02-06 05:19:35 -0200

    easy

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+function compilaSass(){
+    return gulp
+    .src('css/scss/**/*.scss')
+    .pipe(sass(
+        {outputStyle: 'compressed'}
+    ))
+    .pipe(gulp.dest('css/'))
+}
+
+gulp.task('sass', compilaSass);
+
+function watch(){
+    gulp.watch('css/scss/**/*.scss', compilaSass);
+}
+gulp.task('default', watch);
---
+++ js/functions.js
@@ -0,0 +1,25 @@
+Visibility.onVisible(function(){
+  setTimeout(function (){
+      $('.hero__title').addClass("animated fadeInDown");
+  }, 400);
+});
+Visibility.onVisible(function(){
+  setTimeout(function (){
+      $('.hero__copy').addClass("animated fadeInDown");
+  }, 800);
+});
+Visibility.onVisible(function(){
+  setTimeout(function (){
+      $('.hero__btn').addClass("animated fadeInDown");
+  }, 1200);
+});
+Visibility.onVisible(function(){
+  setTimeout(function (){
+      $('.discos__img').addClass("animated fadeInLeft");
+  }, 1600);
+});
+Visibility.onVisible(function(){
+  setTimeout(function (){
+      $('.discos__text').addClass("animated fadeInRight");
+  }, 1800);
+});
